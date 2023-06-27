2df9661f79bd18f1c359bb1e11d9a878cc91a823
Fabian <68711515+fabian4613@users.noreply.github.com>
2023-06-27 10:24:17 -0300

    Update start-domain.sh

--- start-domain.sh
+++ start-domain.sh
@@ -11,5 +11,14 @@ rm /tmp/glassfishpwd
 # Detener el dominio
 asadmin stop-domain --domain_name domain1
 
+# Clonar el repositorio de GitHub
+git clone https://github.com/fabian4613/apps-java-glassfish-02.git /tmp/apps-java-glassfish-02
+
+# Copiar los archivos WAR a la carpeta autodeploy
+cp /tmp/apps-java-glassfish-02/*.war $GLASSFISH_HOME/glassfish/domains/domain1/autodeploy/
+
+# Eliminar el repositorio clonado
+rm -rf /tmp/apps-java-glassfish-02
+
 # Iniciar el dominio nuevamente en modo verbose
 asadmin start-domain --verbose
